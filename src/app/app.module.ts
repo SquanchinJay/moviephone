5e5acdf494105ac14709c7ca767f0e02ec045a2a
Jay Allen <allen.jeremiah555@gmail.com>
2017-12-18 23:29:31 -0500

    some experimentation with material and install flex-layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
 
 import { AppComponent } from './app.component';
 import { HttpModule } from '@angular/http';
@@ -20,7 +26,12 @@ import { ReviewPostFormComponent } from './review-post-form/review-post-form.com
     HttpModule,
     BrowserAnimationsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatProgressBarModule,
+    FlexLayoutModule
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
